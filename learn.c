c23fdae6a8e75ecf1126b30452fe2432580278f2
Martin Maina <xmartinmaina@gmail.com>
2025-09-18 14:27:58 +0300

    first commit

---
+++ learn.c
@@ -0,0 +1,21 @@
+// simple program for displaying units
+/*
+Name: Martin Maina
+Reg:  CT100/G/26125/25
+Description: Units display program
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("SPH 2107: ANALOGUE ELECTRONICS \n");
+    printf("SPI 2102: COMPUTER SYSTEMS AND ORGANISATION \n");
+    printf("UCU 2105: FUNDAMENTALS OF ICT \n");
+    printf("SPI 2103: INTRODUCTION TO COMPUTER SYSTEMS \n");
+    printf("SPI 2120: ESSENTIALS OF ACCOUNTING \n");
+    printf("UCU 2101: COMMUNICATION SKILLS \n");
+    printf("SPM 2104: MATHEMATICS FOR SCIENCE \n");
+    printf("SPI 2101: INTRODUCTION TO PROGRAMMING AND ALGORITHMS");
+
+    return 0;
+}
